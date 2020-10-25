3a9f1572994b4ef3a4935f94427734cc140d2a62
Nathan Krasney <natankrasney@gmail.com>
2020-10-25 14:18:59 +0200

    add not found

--- server.js
+++ server.js
@@ -1,19 +1,22 @@
-console.log('app is loading ...');
+console.log("app is loading ...");
 
-function Station(id , name){
-    this.id = id;
-    this.name = name;
+function Station(id, name) {
+  this.id = id;
+  this.name = name;
 }
 
 let stationHealth = new Map();
-let station1 = new Station("1","Haifa"),station2 = new Station("1","Tel-Aviv");
-let ar1 = [true , false] , ar2 = [true , true];
-stationHealth.set(station1,ar1);
-stationHealth.set(station2,ar2);
+let station1 = new Station("1", "Haifa"),
+  station2 = new Station("2", "Tel-Aviv"),
+  station3 = new Station("3", "Jerusalem");
+let ar1 = [true, false],
+  ar2 = [true, true];
+stationHealth.set(station1, ar1);
+stationHealth.set(station2, ar2);
 
 console.log(`stationHealth.size : ${stationHealth.size}`);
 stationHealth.get(station1).push(false);
 
 console.log(stationHealth.get(station1));
 console.log(stationHealth.get(station2));
-
+console.log(stationHealth.get(station3));
