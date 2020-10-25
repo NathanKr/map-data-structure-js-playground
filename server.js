2abfdce6c20231fefb893d8a601eb9eff489276a
Nathan Krasney <natankrasney@gmail.com>
2020-10-25 12:40:24 +0200

    small change

--- server.js
+++ server.js
@@ -7,10 +7,13 @@ function Station(id , name){
 
 let stationHealth = new Map();
 let station1 = new Station("1","Haifa"),station2 = new Station("1","Tel-Aviv");
-stationHealth.set(station1,[true , false]);
-stationHealth.set(station2,[true , true]);
+let ar1 = [true , false] , ar2 = [true , true];
+stationHealth.set(station1,ar1);
+stationHealth.set(station2,ar2);
 
 console.log(`stationHealth.size : ${stationHealth.size}`);
+stationHealth.get(station1).push(false);
 
 console.log(stationHealth.get(station1));
 console.log(stationHealth.get(station2));
+
